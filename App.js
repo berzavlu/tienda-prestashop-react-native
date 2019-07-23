6dc9e648dadac22306d8b30ddab531864eef4d82
Luis <Luis@MacBook-Pro-de-OnlineLuis.local>
2019-07-23 11:40:26 -0500

    removed logs

--- App.js
+++ App.js
@@ -1,33 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  Image,
-  Alert,
-  TouchableOpacity,
-  StatusBar,
-  Dimensions
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import React from 'react'
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation'
 import router from './src/routes'
 
 const Tabs = createBottomTabNavigator(router, {
